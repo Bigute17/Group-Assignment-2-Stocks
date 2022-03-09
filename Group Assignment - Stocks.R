847067cae2bd7c83b34ae6f943d45199bb05a3d5
jmoor157 <jmoor157@vols.utk.edu>
2022-03-09 12:15:41 -0500

    Stocks- closing price

--- Group Assignment - Stocks.R
+++ Group Assignment - Stocks.R
@@ -6,10 +6,18 @@ stocks <- read_csv('nyse_stocks.csv.zip')
 
 
 ui <- fluidPage(
+  textInput("text", label = h3("Stock Symbol"), value = "Enter Stock Symbol"),
+  plotOutput('plot')
   
 )
 
 server <- function(input, output, session) {
+  output$plot <- renderPlot({
+    stocks %>% 
+      filter(symbol ==   input$text ) %>% 
+      autoplot(close) +
+      labs(title =  input$text)
+  })
   
 }
 
